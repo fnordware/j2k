ddd0f59c4e71c06e70a747de8586e67aad68823a
Brendan Bolles <brendan@fnordware.com>
2016-10-18 11:36:11 -0700

    Added GPL license

--- src/common/j2k_rgba_file.cpp
+++ src/common/j2k_rgba_file.cpp
@@ -1,11 +1,25 @@
-/*
- *  j2k_rgba_file.cpp
- *  j2k_AE
- *
- *  Created by Brendan Bolles on 10/12/16.
- *  Copyright 2016 fnord. All rights reserved.
- *
- */
+
+/* ---------------------------------------------------------------------
+// 
+// j2k - JPEG 2000 plug-ins for Adobe programs
+// Copyright (c) 2002-2016 Brendan Bolles and Aaron Boxer
+// 
+// This file is part of j2k.
+//
+// j2k is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// -------------------------------------------------------------------*/
 
 #include "j2k_rgba_file.h"
 
--- src/common/mac/j2k_OutUI_Carbon.cpp
+++ src/common/mac/j2k_OutUI_Carbon.cpp
@@ -1,5 +1,26 @@
-// j2k
+
+// ---------------------------------------------------------------------
+// 
+// j2k - JPEG 2000 plug-ins for Adobe programs
+// Copyright (c) 2002-2016 Brendan Bolles and Aaron Boxer
+// 
+// This file is part of j2k.
 //
+// j2k is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// ---------------------------------------------------------------------
+
 // Carbon dialog
 
 
